f39f22220bb5fab6770042fb415aedd5b72f47e6
Saurav Yadav <ysaurav777@gmail.com>
2024-04-13 14:30:34 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,53 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react'
 
-function App() {
+const App = () => {
+  const [typecolor,settypecolor]=useState("hex");
+  const [color,setcolor]=useState("#000000");
+
+  function randomnumber(length) {
+    return Math.floor(Math.random()*length);
+  }
+
+  function setcolorhex() {
+    const h = [1,2,3,4,5,6,7,8,9,"A","B","C","D","E","F"];
+    let hcolor="#";
+    for(let i=0;i<6;i++) {
+      hcolor+=h[randomnumber(h.length)];
+    }
+    setcolor(hcolor);
+  }
+
+  function setcolorrdb() {
+    const a=randomnumber(256);
+    const b=randomnumber(256);
+    const c=randomnumber(256);
+
+    setcolor(`rgb(${a},${b},${c})`);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={{
+      width:"100vw",
+      height:"1000vh",
+      backgroundColor:color
+    }}>
+      <button onClick={()=>settypecolor("hex")}>Color from Hex</button>
+      <button onClick={()=>settypecolor("rgb")}>Color from RBG</button>
+      <button onClick={
+        typecolor==="hex"?setcolorhex:setcolorrdb
+      }>Generate Color</button>
+      <div style={{
+        display:"flex",
+        justifyContent:"center",
+        alignItems:"center",
+        color:"white",
+        fontSize:"5rem"
+      }}>
+        <h5>{typecolor==='hex'?"HEX Color: ":"RGB Color: "}</h5>
+        <h3>{color}</h3>
+      </div>
     </div>
-  );
+  )
 }
 
 export default App;
